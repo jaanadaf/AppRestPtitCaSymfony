2f72bbd40a3a4251ad6b2b65912206ea73180efe
jaanadaf <abdelkaderjaadar@yahoo.fr>
2024-11-26 22:04:32 +0100

    Utiliser PHPUnit, Tests unitaires, Tests fonctionnels

---
+++ tests/Controller/SmokeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SmokeTest extends WebTestCase
+{
+    /*public function testApiDocUrlIsSuccessful(): void
+    {
+        $client = self::createClient();
+        $client->followRedirects(false);
+        $client->request('GET', '/api/doc');
+
+        self::assertResponseIsSuccessful();
+    }*/
+
+    public function testApiAccountUrlIsSecure(): void
+    {
+        $client = self::createClient();
+        $client->followRedirects(false);
+        $client->request('GET', '/api/account/me');
+
+        self::assertResponseStatusCodeSame(401);
+    }
+}
