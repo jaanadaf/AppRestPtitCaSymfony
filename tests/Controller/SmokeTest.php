b68a20060632d99690ac0eec1235a11f7ad15f54
jaanadaf <abdelkaderjaadar@yahoo.fr>
2024-11-27 15:38:27 +0100

    Installer DoctrineFixturesBundle et Créer sa première fixture

--- tests/Controller/SmokeTest.php
+++ tests/Controller/SmokeTest.php
@@ -6,15 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SmokeTest extends WebTestCase
 {
-    /*public function testApiDocUrlIsSuccessful(): void
-    {
-        $client = self::createClient();
-        $client->followRedirects(false);
-        $client->request('GET', '/api/doc');
-
-        self::assertResponseIsSuccessful();
-    }*/
-
     public function testApiAccountUrlIsSecure(): void
     {
         $client = self::createClient();
